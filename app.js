5f91d1481ee0c7520b5f9f5c8a5bc72701422eff
Anderson Juhasc <anderson.juhasc@gmail.com>
2015-09-07 13:25:37 -0300

    improving class

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 var Bitex = require('./bitex')
   , env = require('dotenv').load();
 
-var bot = new Bitex(process.env.API_KEY, process.env.API_PASS, process.env.API_SECRET);
+var bot = new Bitex(process.env.BROKER, process.env.API_KEY, process.env.API_PASS, process.env.API_SECRET);
--- bitex.js
+++ bitex.js
@@ -1,24 +1,35 @@
 var WebSocket = require('ws');
 
-// Constructor
-function Bitex(user, pass, second_factor) {
+/**
+ * Constructor
+ * @param {number} broker_id
+ * @param {string} api_key
+ * @param {string} api_pass
+ * @param {string} opt_second_factor
+ */
+function Bitex(broker_id, api_key, api_pass, opt_second_factor) {
     var self = this
-      , count = 0
-      , p = 0
       , ws = new WebSocket('wss://api.blinktrade.com/trade/');
 
-    this.user = user; //api-key
-    this.pass = pass; //api-pass
-    this.opt_second_factor = second_factor; //api secret
+    this.broker = broker_id;
+    this.api_key = api_key;
+    this.api_pass = api_pass;
+    this.opt_second_factor = opt_second_factor; //api secret
     this.fingerprint = parseInt(Math.random() * 1000000, 10); //random integer
     this.ws = ws;
 
     ws.on('open', function() {
+        console.log('OPEN CONNECTION');
+
         self.testRequest();
     });
 
     ws.on('message', function(message) {
         console.log(message);
+    });
+
+    ws.on('close', function() {
+        console.log('CLOSE CONNECTION');
     });
 }
 
@@ -28,80 +39,75 @@ function Bitex(user, pass, second_factor) {
  */
 Bitex.prototype.testRequest = function(opt_requestId) {
     var self = this
-      , date = new Date()
-      , reqId = opt_requestId || date.getTime()
+      , d = new Date()
+      , requestId = opt_requestId || d.getTime()
       , msg = {
         'MsgType': '1',
-        'TestReqID': reqId,
-        'SendTime': date.getTime(),
+        'TestReqID': requestId,
+        'SendTime': d.getTime(),
     };
 
     self.ws.send(JSON.stringify(msg));
 }
 
 /**
- * @param {Array.<string>} symbols
+ * @param {Array.<string>} symbols ['BTCBRL', 'BTCVEF', ...]
  * @param {string} opt_requestId. Defaults to random generated number
  * @return {number}
  */
 Bitex.prototype.subscribeSecurityStatus = function(symbols, opt_requestId) {
-  var self = this;
-  var requestId = opt_requestId || parseInt( 1e7 * Math.random() , 10 );
+    var self = this
+      , requestId = opt_requestId || parseInt(1e7 * Math.random() , 10)
+      , msg = {
+        'MsgType': 'e',
+        'SecurityStatusReqID': requestId,
+        'SubscriptionRequestType': '1',
+        'Instruments': symbols
+    };
 
-  var msg = {
-    'MsgType': 'e',
-    'SecurityStatusReqID': requestId,
-    'SubscriptionRequestType': '1',
-    'Instruments': symbols
-  };
+    self.ws.send(JSON.stringify(msg));
 
-  self.ws.send(JSON.stringify(msg));
-
-  return requestId;
-};
+    return requestId;
+}
 
 /**
  * @param {string} opt_market. Defaults to BLINK
  * @param {string} opt_requestId. Defaults to random generated number
  */
-Bitex.prototype.requestSecurityList = function(opt_market, opt_requestId){
-  var self = this;
-  var requestId = opt_requestId || parseInt( 1e7 * Math.random() , 10 );
-  var market = opt_market || "BLINK";
-  var msg = {
-    'MsgType': 'x',
-    'SecurityReqID': requestId,
-    'SecurityListRequestType': 0, // Symbol
-    'Market': market,
-    'SecurityRequestResult': 0
-  };
+Bitex.prototype.requestSecurityList = function(opt_market, opt_requestId) {
+    var self = this
+      , requestId = opt_requestId || parseInt( 1e7 * Math.random() , 10 )
+      , market = opt_market || "BLINK"
+      , msg = {
+        'MsgType': 'x',
+        'SecurityReqID': requestId,
+        'SecurityListRequestType': 0, // Symbol
+        'Market': market,
+        'SecurityRequestResult': 0
+    };
 
-  self.ws.send(JSON.stringify(msg));
-};
+    self.ws.send(JSON.stringify(msg));
+}
 
 
 /**
- * @param {number} brokerID
- * @param {string} username
- * @param {string} password
- * @param {string=} opt_second_factor
  * @param {number=} opt_request_id
  */
-Bitex.prototype.login = function(brokerID, username, password, opt_second_factor, opt_request_id) {
+Bitex.prototype.login = function(opt_request_id) {
     var self = this
       , reqId = opt_request_id || parseInt(Math.random() * 1000000, 10)
       , msg = {
         'MsgType': 'BE',
         'UserReqID': reqId,
-        'BrokerID': brokerID,
-        'Username': username,
-        'Password': password,
+        'BrokerID': self.broker,
+        'Username': self.api_key,
+        'Password': self.api_pass,
         'FingerPrint': self.fingerprint,
         'UserReqTyp': '1'
     };
 
     if (typeof opt_second_factor !== 'undefined' && typeof opt_second_factor === 'number') {
-        msg['SecondFactor'] = opt_second_factor;
+        msg['SecondFactor'] = self.opt_second_factor;
     }
 
     self.ws.send(JSON.stringify(msg));
